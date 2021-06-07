ea303970f56354ba4fd2525e4e508845799fb31e
Salaudeen Ismail <ismailsalaudeen6696@gmail.com>
2021-06-07 16:07:04 +0100

    api issue

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import credentials from './Credentials';
+// import axios from 'axios';
+
+
+
+const LocationInput = () => {
+  const [location, setLocation] = React.useState();
+
+  const getWeather = async () => {
+    const url = `api.openweathermap.org/data/2.5/weather?q=${location}&appid=${credentials.api_Key}`;
+
+
+    try {
+      console.log('URRRLLLLL', url);
+      await fetch(url).then(async response => {
+        try {
+          const data = response;
+          console.log('response data?', data)
+        } catch (error) {
+          console.log('Error happened here!')
+          console.error(error)
+        }
+      });
+
+    } catch (error) {
+      console.log("Error in get call!!");
+
+    }
+
+
+  }
+
+  return (
+
+    <div>
+      <input
+        type="text"
+        placeholder="Enter Location here..."
+        onChange={(e) => setLocation(e.target.value)}
+      >
+      </input>
+
+      <button
+        onClick={getWeather}>
+        Get Weather
+      </button>
+    </div>
+  )
+}
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <LocationInput />
+      I'm still working
     </div>
   );
 }
---
+++ src/Credentials.js
@@ -0,0 +1,7 @@
+
+
+const credentials = {
+  api_Key: '4a493c1fc0363a08676f474c7edc0928'
+}
+
+export default credentials;
